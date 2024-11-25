21019316f2d88364674034c469c03e5f7dc85e30
szekelya122 <szekelyarmin121@gmail.com>
2024-11-25 11:24:46 +0100

    Added data
    created users

--- webshop.sql
+++ webshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Nov 25, 2024 at 09:51 AM
+-- Generation Time: Nov 25, 2024 at 10:23 AM
 -- Server version: 5.7.24
 -- PHP Version: 8.3.1
 
@@ -20,6 +20,71 @@ SET time_zone = "+00:00";
 --
 -- Database: `webshop`
 --
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `DeleteOrder` (IN `p_order_id` INT)   BEGIN
+    DELETE FROM orders
+    WHERE id = p_order_id;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GetOrdersByUser` (IN `p_user_id` INT)   BEGIN
+    SELECT 
+        o.id AS order_id,
+        u.username,
+        p.product_name,
+        o.order_quantity,
+        o.order_date,
+        o.status
+    FROM orders o
+    JOIN users u ON o.user_id = u.user_id
+    JOIN product p ON o.product_id = p.product_id
+    WHERE o.user_id = p_user_id;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GetProductsByCategory` (IN `p_category_id` INT)   BEGIN
+    SELECT product_id, product_name, price, product_quantity
+    FROM product
+    WHERE category_id = p_category_id;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `InsertUser` (IN `p_username` VARCHAR(100), IN `p_role` VARCHAR(50), IN `p_address` VARCHAR(255), IN `p_phonenumber` VARCHAR(15), IN `p_email` VARCHAR(100))   BEGIN
+    INSERT INTO users (username, role, address, phonenumber, email)
+    VALUES (p_username, p_role, p_address, p_phonenumber, p_email);
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `PlaceOrder` (IN `p_user_id` INT, IN `p_product_id` INT, IN `p_quantity` INT)   BEGIN
+    DECLARE v_product_quantity INT;
+
+    -- Check product availability
+    SELECT product_quantity INTO v_product_quantity
+    FROM product
+    WHERE product_id = p_product_id;
+
+    IF v_product_quantity >= p_quantity THEN
+        -- Place the order
+        INSERT INTO orders (user_id, product_id, order_quantity, status)
+        VALUES (p_user_id, p_product_id, p_quantity, 'Processing');
+
+        -- Update product stock
+        UPDATE product
+        SET product_quantity = product_quantity - p_quantity
+        WHERE product_id = p_product_id;
+    ELSE
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Insufficient stock to place the order';
+    END IF;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `UpdateProductPrice` (IN `p_product_id` INT, IN `p_new_price` DECIMAL(10,2))   BEGIN
+    UPDATE product
+    SET price = p_new_price
+    WHERE product_id = p_product_id;
+END$$
+
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -79,6 +144,14 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `role`, `created_at`, `address`, `phonenumber`, `email`) VALUES
+(2, 'Gipsz Jakab', 'Customer', '2024-11-25 10:16:53', 'Lehel sor 2', '06202369229', 'gipszjakab1976@gmail.com'),
+(3, 'Hologram Ákos', 'Customer', '2024-11-25 10:17:50', 'Lehel sor 3', '06202369220', 'Hmi@gmail.com');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -136,7 +209,7 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
