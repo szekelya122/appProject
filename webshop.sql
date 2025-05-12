db602f78e5fba0dc497d6cc03136f11132eb689f
szekelya122 <szekelyarmin121@gmail.com>
2025-05-12 21:18:12 +0200

    dokumentáció, sql dump

--- webshop.sql
+++ webshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 19, 2025 at 08:12 PM
+-- Generation Time: May 12, 2025 at 07:10 PM
 -- Server version: 5.7.24
 -- PHP Version: 8.3.1
 
@@ -139,7 +139,7 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `GetUserLastFiveOrders` (IN `user_id
         o.order_date,
         o.status,
         oi.quantity,
-        oi.price AS ordered_price,
+        oi.ordered_price AS ordered_price,
         p.name AS product_name
     FROM
         orders o
@@ -267,18 +267,16 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`id`, `user_id`, `order_date`, `status`, `total_amount`, `shipping_name`, `shipping_address`, `shipping_city`, `shipping_zip`, `shipping_country`, `price`) VALUES
-(21, 38, '2025-04-19 16:02:38', NULL, 30, 'Székely Ármin', 'Árpád utca 44', 'Dombóvár', '7200', 'Hungary', NULL),
-(22, 38, '2025-04-19 16:03:01', NULL, 29, 'Székely Ármin', 'Árpád utca 44', 'Dombóvár', '7200', 'Hungary', NULL),
-(23, 38, '2025-04-19 16:07:49', NULL, 500, 'Székely Ármin', 'Árpád utca 44', 'Dombóvár', '7200', 'Hungary', NULL),
-(24, 38, '2025-04-19 16:56:32', NULL, 500, 'Székely Ármin', 'Árpád utca 44', 'Dombóvár', '7200', 'Hungary', NULL),
-(25, 38, '2025-04-19 18:01:45', 'pending', NULL, 'Székely Ármin', 'Árpád utca 44', 'Dombóvár', '7200', 'Hungary', 40),
-(26, 38, '2025-04-19 18:38:13', 'pending', NULL, 'Székely Ármin', 'Árpád utca 44', 'Dombóvár', '7200', 'Hungary', NULL),
-(27, 39, '2025-04-19 18:51:57', 'pending', NULL, 'Teszt', 'Teszt', 'teszt', '123', 'Teszt', 40),
-(28, 39, '2025-04-19 18:52:26', 'pending', NULL, 'Teszt', 'Teszt', 'teszt', '123', 'Teszt', NULL),
-(29, 39, '2025-04-19 18:54:51', 'pending', NULL, 'Teszt', 'Teszt', 'teszt', '123', 'Teszt', NULL),
-(30, 39, '2025-04-19 19:06:24', 'pending', NULL, 'Teszt', 'Teszt', 'teszt', '123', 'Teszt', NULL),
-(31, 38, '2025-04-19 20:06:16', 'pending', NULL, 'Székely Ármin', 'Árpád utca 44', 'Dombóvár', '7200', 'Hungary', 500),
-(32, 38, '2025-04-19 20:11:06', 'pending', NULL, 'Székely Ármin', 'Árpád utca 44', 'Dombóvár', '7200', 'Hungary', NULL);
+(51, 38, '2025-04-19 21:30:08', 'pending', 0, 'Székely Ármin', 'Árpád utca 44', 'Dombóvár', '7200', 'Hungary', NULL),
+(52, 38, '2025-04-19 21:31:00', 'pending', 0, 'Székely Ármin', 'Árpád utca 44', 'Dombóvár', '7200', 'Hungary', NULL),
+(53, 38, '2025-04-19 21:31:45', 'pending', 9, 'Székely Ármin', 'Árpád utca 44', 'Dombóvár', '7200', 'Hungary', NULL),
+(54, 38, '2025-04-19 21:33:23', 'pending', 1120, 'Székely Ármin', 'Árpád utca 44', 'Dombóvár', '7200', 'Hungary', NULL),
+(55, 38, '2025-04-19 21:37:18', 'pending', 0, 'Székely Ármin', 'Árpád utca 44', 'Dombóvár', '7200', 'Hungary', NULL),
+(56, 38, '2025-05-12 19:06:58', 'pending', 40, 'Székely Ármin', 'Árpád utca 44', 'Dombóvár', '7200', 'Hungary', NULL),
+(57, 38, '2025-05-12 19:07:15', 'pending', 40, 'Székely Ármin', 'Árpád utca 44', 'Dombóvár', '7200', 'Hungary', NULL),
+(58, 38, '2025-05-12 19:07:24', 'pending', 40, 'Székely Ármin', 'Árpád utca 44', 'Dombóvár', '7200', 'Hungary', NULL),
+(59, 38, '2025-05-12 19:07:38', 'pending', 40, 'Székely Ármin', 'Árpád utca 44', 'Dombóvár', '7200', 'Hungary', NULL),
+(60, 38, '2025-05-12 19:08:12', 'pending', 40, 'Székely Ármin', 'Árpád utca 44', 'Dombóvár', '7200', 'Hungary', NULL);
 
 -- --------------------------------------------------------
 
@@ -291,21 +289,18 @@ CREATE TABLE `order_items` (
   `order_id` int(11) NOT NULL,
   `product_id` int(11) UNSIGNED DEFAULT NULL,
   `quantity` int(11) NOT NULL,
-  `price` decimal(10,2) NOT NULL
+  `ordered_price` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `order_items`
 --
 
-INSERT INTO `order_items` (`id`, `order_id`, `product_id`, `quantity`, `price`) VALUES
-(1, 21, 8, 1, '30.00'),
-(2, 22, 13, 1, '29.00'),
-(3, 23, 9, 1, '500.00'),
-(4, 24, 9, 1, '500.00'),
-(5, 25, 10, 1, '40.00'),
-(6, 27, 10, 1, '40.00'),
-(7, 31, 9, 1, '500.00');
+INSERT INTO `order_items` (`id`, `order_id`, `product_id`, `quantity`, `ordered_price`) VALUES
+(9, 54, 9, 1, '500.00'),
+(10, 54, 10, 1, '40.00'),
+(11, 54, 13, 20, '29.00'),
+(12, 60, 10, 1, '40.00');
 
 -- --------------------------------------------------------
 
@@ -329,12 +324,14 @@ CREATE TABLE `product` (
 
 INSERT INTO `product` (`id`, `name`, `price`, `quantity`, `category_id`, `img_path`, `description`) VALUES
 (8, 'Gyűrű', '30.00', 0, 1, '../backend/uploads/6803c8678dd39.jpg', NULL),
-(9, 'Gyűrű 2', '500.00', 40, 1, '../backend/uploads/6803c876553cf.jpg', NULL),
-(10, 'Gyűrű 3', '40.00', 26, 1, '../backend/uploads/6803c8838e980.jpg', NULL),
+(9, 'Gyűrű 2', '500.00', 0, 1, '../backend/uploads/6803c876553cf.jpg', NULL),
+(10, 'Gyűrű 3', '40.00', 1, 1, '../backend/uploads/6803c8838e980.jpg', NULL),
 (11, 'Nyaklánc', '399.00', 1, 2, '../backend/uploads/6803c8981baf7.jpg', NULL),
 (12, 'Nyaklánc 2', '59.00', 2, 2, '../backend/uploads/6803c8ad5e0f3.jpg', NULL),
-(13, 'Nyaklánc 3', '29.00', 40, 2, '../backend/uploads/6803c8caa1fb3.jpg', NULL),
-(14, 'Gyűrű321321', '3.00', 16, 1, '../backend/uploads/6803d17fe326f.jpg', NULL);
+(13, 'Nyaklánc 3', '29.00', 20, 2, '../backend/uploads/6803c8caa1fb3.jpg', NULL),
+(15, 'Karóra', '40.00', 10, 3, '../backend/uploads/6804185f89426.jpg', NULL),
+(16, 'Karóra 2', '5000.00', 1, 3, '../backend/uploads/680418711a638.jpg', NULL),
+(17, 'Karóra 3', '8900000.00', 3, 3, '../backend/uploads/680418d0d91bf.jpg', NULL);
 
 -- --------------------------------------------------------
 
@@ -424,7 +421,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `categories`
@@ -436,19 +433,19 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
 
 --
 -- AUTO_INCREMENT for table `order_items`
 --
 ALTER TABLE `order_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `users`
